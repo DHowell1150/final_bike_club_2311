8db206a519468f45f1704a8e4e7df6c6fc2fc656
Dana Howell <howelld115@gmail.com>
2024-01-16 09:12:49 -0700

    I1 feature: Initialize Ride with attributes including #loop?

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,20 @@
 class Ride
 
+  attr_reader :name, 
+              :distance, 
+              :terrain,
+              :loop,
+              :total_distance
+
+  def initialize(data)
+    @name = data[:name]
+    @distance = data[:distance]
+    @terrain = data[:terrain]
+    @loop = false
+    @total_distance = 21.4
+  end
+
+  def loop?
+    @loop
+  end
 end
